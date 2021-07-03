ae977281db5ada7810ff8f4f8caf9df782e972e2
hyuna1004 <40584381+hyuna1004@users.noreply.github.com>
2021-07-03 22:28:47 +0900

    2231 분해합
    2231 분해합

---
+++ boj/brute force/2231.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int arr[101];
+int main() {
+	int N;
+	int result = 0;
+	cin >> N;
+
+	for (int i = 1; i < N; i++) {
+		int sum = 0;
+		int num = i;
+		while (num!= 0) {
+			sum += num % 10;
+			num /= 10;
+		}
+		if (sum + i == N) {
+			result = i;
+			break;
+		}
+	}
+
+	cout << result;
+
+	return 0;
+}
